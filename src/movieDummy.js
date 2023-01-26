ae816a88fe792c346d240f0348bae9ad5afbee5f
jjang3530 <jayjangdev@gmail.com>
2023-01-26 13:23:17 -0500

    Update Movie data and table

---
+++ src/movieDummy.js
@@ -0,0 +1,402 @@
+export const dummy = {
+        "dates": {
+        "maximum": "2023-01-31",
+        "minimum": "2022-12-14"
+        },
+        "page": 1,
+        "results": [
+        {
+        "adult": false,
+        "backdrop_path": "/faXT8V80JRhnArTAeYXz0Eutpv9.jpg",
+        "genre_ids": [
+        16,
+        28,
+        12,
+        35,
+        10751,
+        14
+        ],
+        "id": 315162,
+        "original_language": "en",
+        "original_title": "Puss in Boots: The Last Wish",
+        "overview": "Puss in Boots discovers that his passion for adventure has taken its toll: He has burned through eight of his nine lives, leaving him with only one life left. Puss sets out on an epic journey to find the mythical Last Wish and restore his nine lives.",
+        "popularity": 6201.733,
+        "poster_path": "/kuf6dutpsT0vSVehic3EZIqkOBt.jpg",
+        "release_date": "2022-12-21",
+        "title": "Puss in Boots: The Last Wish",
+        "video": false,
+        "vote_average": 8.6,
+        "vote_count": 2587
+        },
+        {
+        "adult": false,
+        "backdrop_path": "/q2fY4kMXKoGv4CQf310MCxpXlRI.jpg",
+        "genre_ids": [
+        878,
+        27,
+        35
+        ],
+        "id": 536554,
+        "original_language": "en",
+        "original_title": "M3GAN",
+        "overview": "A brilliant toy company roboticist uses artificial intelligence to develop M3GAN, a life-like doll programmed to emotionally bond with her newly orphaned niece. But when the doll's programming works too well, she becomes overprotective of her new friend with terrifying results.",
+        "popularity": 6064.293,
+        "poster_path": "/rxDPzExeovcBZY2IVWdYs87AzVE.jpg",
+        "release_date": "2023-01-06",
+        "title": "M3GAN",
+        "video": false,
+        "vote_average": 7.4,
+        "vote_count": 631
+        },
+        {
+        "adult": false,
+        "backdrop_path": "/s16H6tpK2utvwDtzZ8Qy4qm5Emw.jpg",
+        "genre_ids": [
+        878,
+        12,
+        28
+        ],
+        "id": 76600,
+        "original_language": "en",
+        "original_title": "Avatar: The Way of Water",
+        "overview": "Set more than a decade after the events of the first film, learn the story of the Sully family (Jake, Neytiri, and their kids), the trouble that follows them, the lengths they go to keep each other safe, the battles they fight to stay alive, and the tragedies they endure.",
+        "popularity": 2478.474,
+        "poster_path": "/t6HIqrRAclMCA60NsSmeqe9RmNV.jpg",
+        "release_date": "2022-12-16",
+        "title": "Avatar: The Way of Water",
+        "video": false,
+        "vote_average": 7.7,
+        "vote_count": 4612
+        },
+        {
+        "adult": false,
+        "backdrop_path": "/mYQSbZdGX0JJb49NLO04UA0iFEU.jpg",
+        "genre_ids": [
+        28,
+        35,
+        80
+        ],
+        "id": 1015963,
+        "original_language": "en",
+        "original_title": "High Heat",
+        "overview": "When the local mafia shows up to burn down her restaurant, Ana, a chef with a meticulous past, defends her turf and proves her knife skills both in and out of the kitchen.",
+        "popularity": 826.764,
+        "poster_path": "/mmD0NVdhiRiCu64YKem5GK5PSfy.jpg",
+        "release_date": "2022-12-16",
+        "title": "High Heat",
+        "video": false,
+        "vote_average": 5.4,
+        "vote_count": 31
+        },
+        {
+        "adult": false,
+        "backdrop_path": "/b08BDQPq42AoLMfhi7DtTOoYqVu.jpg",
+        "genre_ids": [
+        9648,
+        80,
+        27
+        ],
+        "id": 800815,
+        "original_language": "en",
+        "original_title": "The Pale Blue Eye",
+        "overview": "West Point, New York, 1830. When a cadet at the burgeoning military academy is found hanged with his heart cut out, the top brass summons former New York City constable Augustus Landor to investigate. While attempting to solve this grisly mystery, the reluctant detective engages the help of one of the cadets: a strange but brilliant young fellow by the name of Edgar Allan Poe",
+        "popularity": 722.523,
+        "poster_path": "/9xkGlFRqrN8btTLU0KQvOfn2PHr.jpg",
+        "release_date": "2022-12-23",
+        "title": "The Pale Blue Eye",
+        "video": false,
+        "vote_average": 7,
+        "vote_count": 836
+        },
+        {
+        "adult": false,
+        "backdrop_path": "/jJbEKl69hgNdQ9vQISqQGEDx4Lh.jpg",
+        "genre_ids": [
+        35
+        ],
+        "id": 632065,
+        "original_language": "en",
+        "original_title": "House Party",
+        "overview": "Aspiring club promoters and best buds Damon and Kevin are barely keeping things together. Out of money, down on their luck and about to lose the roofs over their heads—and freshly fired from their low-lift jobs as house cleaners—the pair needs a huge windfall to make their problems go away. In a ‘what the hell?’ move, they decide to host the party of the year at an exclusive mansion, the site of their last cleaning job, which just happens to belong to none other than LeBron James. No permission? No problem. What could go wrong?",
+        "popularity": 688.035,
+        "poster_path": "/KiyKR9m6h01eIvGObGmpt16U3F.jpg",
+        "release_date": "2023-01-13",
+        "title": "House Party",
+        "video": false,
+        "vote_average": 7.2,
+        "vote_count": 19
+        },
+        {
+        "adult": false,
+        "backdrop_path": "/2uDxaZ16nEsT9KJQMAYLP54tnjW.jpg",
+        "genre_ids": [
+        9648,
+        53,
+        80,
+        18
+        ],
+        "id": 1043565,
+        "original_language": "en",
+        "original_title": "Mindcage",
+        "overview": "Detectives Jake Doyle and Mary Kelly seek the help of an incarcerated serial killer named The Artist when a copycat killer strikes. While Mary searches for clues in The Artist's brilliant but twisted psyche, she and Jake are lured into a diabolical game of cat and mouse, racing against time to stay one step ahead of The Artist and his copycat.",
+        "popularity": 525.453,
+        "poster_path": "/JvqaHXdKBtEAmm4EDVTGkoAGIj.jpg",
+        "release_date": "2022-12-16",
+        "title": "Mindcage",
+        "video": false,
+        "vote_average": 7.4,
+        "vote_count": 72
+        },
+        {
+        "adult": false,
+        "backdrop_path": "/gwRMwljwHoNCyyJVgMA2XC4qPUU.jpg",
+        "genre_ids": [
+        18,
+        35
+        ],
+        "id": 615777,
+        "original_language": "en",
+        "original_title": "Babylon",
+        "overview": "A tale of outsized ambition and outrageous excess, tracing the rise and fall of multiple characters in an era of unbridled decadence and depravity during Hollywood's transition from silent films and to sound films in the late 1920s.",
+        "popularity": 344.297,
+        "poster_path": "/wjOHjWCUE0YzDiEzKv8AfqHj3ir.jpg",
+        "release_date": "2022-12-23",
+        "title": "Babylon",
+        "video": false,
+        "vote_average": 7.8,
+        "vote_count": 401
+        },
+        {
+        "adult": false,
+        "backdrop_path": "/qAatLCuRhvLyNJbH5IGmxLyU3tx.jpg",
+        "genre_ids": [
+        16,
+        14,
+        12
+        ],
+        "id": 876792,
+        "original_language": "ja",
+        "original_title": "劇場版 転生したらスライムだった件 紅蓮の絆編",
+        "overview": "Corporate worker Mikami Satoru is stabbed by a random killer, and is reborn to an alternate world. But he turns out to be reborn a slime!  Thrown into this new world with the name Rimuru, he begins his quest to create a world that’s welcoming to all races.",
+        "popularity": 206.8,
+        "poster_path": "/9nYH8JGbIvXVaNX7f7tya8QUm3e.jpg",
+        "release_date": "2023-01-20",
+        "title": "That Time I Got Reincarnated as a Slime the Movie: Scarlet Bond",
+        "video": false,
+        "vote_average": 7.7,
+        "vote_count": 11
+        },
+        {
+        "adult": false,
+        "backdrop_path": "/h8x3O4rqNyTfUtaYBaUj96UEv66.jpg",
+        "genre_ids": [
+        35,
+        18
+        ],
+        "id": 937278,
+        "original_language": "en",
+        "original_title": "A Man Called Otto",
+        "overview": "Otto is a grump who's given up on life following the loss of his wife and wants to end it all. When a young family moves in nearby, he meets his match in quick-witted Marisol, leading to a friendship that will turn his world around.",
+        "popularity": 206.417,
+        "poster_path": "/d7uLLwLsRwwQlTigSBnPj4CU2sP.jpg",
+        "release_date": "2022-12-30",
+        "title": "A Man Called Otto",
+        "video": false,
+        "vote_average": 7.4,
+        "vote_count": 70
+        },
+        {
+        "adult": false,
+        "backdrop_path": "/d3l7kgFJyLTTQSrR4ysCk5yeVyW.jpg",
+        "genre_ids": [
+        28,
+        18
+        ],
+        "id": 579974,
+        "original_language": "te",
+        "original_title": "రౌద్రం రణం రుధిరం",
+        "overview": "A fictional history of two legendary revolutionaries' journey away from home before they began fighting for their country in the 1920s.",
+        "popularity": 161.931,
+        "poster_path": "/u0XUBNQWlOvrh0Gd97ARGpIkL0.jpg",
+        "release_date": "2022-06-01",
+        "title": "RRR",
+        "video": false,
+        "vote_average": 7.7,
+        "vote_count": 619
+        },
+        {
+        "adult": false,
+        "backdrop_path": "/9Rq14Eyrf7Tu1xk0Pl7VcNbNh1n.jpg",
+        "genre_ids": [
+        28,
+        53
+        ],
+        "id": 646389,
+        "original_language": "en",
+        "original_title": "Plane",
+        "overview": "After a heroic job of successfully landing his storm-damaged aircraft in a war zone, a fearless pilot finds himself between the agendas of multiple militias planning to take the plane and its passengers hostage.",
+        "popularity": 142.43,
+        "poster_path": "/2g9ZBjUfF1X53EinykJqiBieUaO.jpg",
+        "release_date": "2023-01-13",
+        "title": "Plane",
+        "video": false,
+        "vote_average": 6.6,
+        "vote_count": 18
+        },
+        {
+        "adult": false,
+        "backdrop_path": "/fWTeGzbPYJSwmE0stZVmNPyaAnk.jpg",
+        "genre_ids": [
+        18,
+        10749
+        ],
+        "id": 622082,
+        "original_language": "en",
+        "original_title": "In from the Side",
+        "overview": "Two men in a gay men's rugby club must conceal an adulterous affair they unwittingly fall into before it leads to the collapse of the delicate social and political fabric of the club.",
+        "popularity": 118.424,
+        "poster_path": "/aGsDeDtT4HJ2v3wDHy5ZDEXlRiA.jpg",
+        "release_date": "2023-01-20",
+        "title": "In from the Side",
+        "video": false,
+        "vote_average": 6.8,
+        "vote_count": 20
+        },
+        {
+        "adult": false,
+        "backdrop_path": "/mvKPZx9fu7GqUfm9sV0rleyYpTU.jpg",
+        "genre_ids": [
+        878,
+        28,
+        18
+        ],
+        "id": 842675,
+        "original_language": "zh",
+        "original_title": "流浪地球2",
+        "overview": "Humans built huge engines on the surface of the earth to find a new home. But the road to the universe is perilous. In order to save earth, young people once again have to step forward to start a race against time for life and death.",
+        "popularity": 83.686,
+        "poster_path": "/pZCamRrMWGotEoZV0y51uVGozXg.jpg",
+        "release_date": "2023-01-22",
+        "title": "The Wandering Earth II",
+        "video": false,
+        "vote_average": 7.9,
+        "vote_count": 14
+        },
+        {
+        "adult": false,
+        "backdrop_path": "/40g1El3PBqjlujIT8wIeRJRponF.jpg",
+        "genre_ids": [
+        28,
+        18,
+        53
+        ],
+        "id": 864692,
+        "original_language": "hi",
+        "original_title": "पठान",
+        "overview": "A soldier caught by enemies and presumed dead comes back to complete his mission, accompanied by old companions and foes.",
+        "popularity": 79.845,
+        "poster_path": "/dPV6iMhltNaU7sOTLm9ytkxlQ1U.jpg",
+        "release_date": "2023-01-25",
+        "title": "Pathaan",
+        "video": false,
+        "vote_average": 7.7,
+        "vote_count": 7
+        },
+        {
+        "adult": false,
+        "backdrop_path": "/tCpMRsHlfR6CcqJYA3kJMS3YApt.jpg",
+        "genre_ids": [
+        28,
+        53,
+        80
+        ],
+        "id": 1035806,
+        "original_language": "en",
+        "original_title": "Detective Knight: Independence",
+        "overview": "Detective James Knight 's last-minute assignment to the Independence Day shift turns into a race to stop an unbalanced ambulance EMT from imperiling the city's festivities. The misguided vigilante, playing cop with a stolen gun and uniform, has a bank vault full of reasons to put on his own fireworks show... one that will strike dangerously close to Knight's home.",
+        "popularity": 71.538,
+        "poster_path": "/jrPKVQGjc3YZXm07OYMriIB47HM.jpg",
+        "release_date": "2023-01-20",
+        "title": "Detective Knight: Independence",
+        "video": false,
+        "vote_average": 5.3,
+        "vote_count": 7
+        },
+        {
+        "adult": false,
+        "backdrop_path": "/ihBrgKx7IuOptUYcblRJHAraRM0.jpg",
+        "genre_ids": [
+        18
+        ],
+        "id": 901563,
+        "original_language": "fr",
+        "original_title": "Close",
+        "overview": "Two thirteen year-olds have always been incredibly close but they drift apart after the intimacy of their relationship is questioned by schoolmates.  An emotionally transformative and unforgettable portrait of the intersection of friendship and love, identity and independence, and heartbreak and healing.",
+        "popularity": 65.359,
+        "poster_path": "/j7ReE0UZ9sgtfLvzszMgQw8Mxb2.jpg",
+        "release_date": "2022-12-02",
+        "title": "Close",
+        "video": false,
+        "vote_average": 7.8,
+        "vote_count": 148
+        },
+        {
+        "adult": false,
+        "backdrop_path": "/fHDvGGPFry65ou79WLi6JsjCZrM.jpg",
+        "genre_ids": [
+        35
+        ],
+        "id": 632856,
+        "original_language": "en",
+        "original_title": "Spirited",
+        "overview": "Each Christmas Eve, the Ghost of Christmas Present selects one dark soul to be reformed by a visit from three spirits. But this season, he picked the wrong Scrooge. Clint Briggs turns the tables on his ghostly host until Present finds himself reexamining his own past, present and future.",
+        "popularity": 62.198,
+        "poster_path": "/h3zAzTMs5EP3cKusOxFNGSFE1WI.jpg",
+        "release_date": "2022-11-10",
+        "title": "Spirited",
+        "video": false,
+        "vote_average": 7.1,
+        "vote_count": 370
+        },
+        {
+        "adult": false,
+        "backdrop_path": "/dom8lzl2iU46nDu6s4lBolBNjQs.jpg",
+        "genre_ids": [
+        18
+        ],
+        "id": 736732,
+        "original_language": "ko",
+        "original_title": "브로커",
+        "overview": "Boxes are left out for people to anonymously drop off their unwanted babies.",
+        "popularity": 58.386,
+        "poster_path": "/ulGCgWnidH9LJTRL9rMV23cLtQb.jpg",
+        "release_date": "2022-12-26",
+        "title": "Broker",
+        "video": false,
+        "vote_average": 7.3,
+        "vote_count": 174
+        },
+        {
+        "adult": false,
+        "backdrop_path": "/lIulkr7fr5rDbbxAuhK29fcjjCP.jpg",
+        "genre_ids": [
+        27,
+        878,
+        53
+        ],
+        "id": 667216,
+        "original_language": "en",
+        "original_title": "Infinity Pool",
+        "overview": "While staying at an isolated island resort, James and Em are enjoying a perfect vacation of pristine beaches, exceptional staff, and soaking up the sun. But guided by the seductive and mysterious Gabi, they venture outside the resort grounds and find themselves in a culture filled with violence, hedonism, and untold horror.",
+        "popularity": 51.471,
+        "poster_path": "/2luUHK8KlrYJkh77hIkLCNQl1A6.jpg",
+        "release_date": "2023-01-27",
+        "title": "Infinity Pool",
+        "video": false,
+        "vote_average": 2,
+        "vote_count": 1
+        }
+        ],
+        "total_pages": 10,
+        "total_results": 193
+        }
